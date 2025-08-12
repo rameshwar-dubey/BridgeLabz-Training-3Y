97d5bb414ab87729c989a2db2030995ebe5ee3ec
rameshwar-dubey <rameshward040@gmail.com>
2025-08-12 13:13:06 +0530

    added addTwoNumbers file

---
+++ src/Assignment/addTwoNumbers.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+import java.util.Scanner;
+public class addTwoNumbers {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = add(a, b);
+        System.out.print(c);
+    }
+}
