f1a25f2c06ea4afa038591b1fb25959cd29603c6
rameshwar-dubey <rameshward040@gmail.com>
2025-08-12 13:21:06 +0530

    added areaCircle file

---
+++ src/Assignment/areaCircle.java
@@ -0,0 +1,12 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class areaCircle {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int rad=sc.nextInt();
+        int area=22/7*rad*rad;
+        System.out.print("area"+area);
+    }
+}
