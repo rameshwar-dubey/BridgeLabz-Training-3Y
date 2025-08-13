da7e822d8a0183acee1dc74e365c75c7e6f62f5f
rameshwar-dubey <rameshward040@gmail.com>
2025-08-13 13:27:39 +0530

    added conversion file

---
+++ src/level1_practice_programe/conersion.java
@@ -0,0 +1,10 @@
+package level1_practice_programe;
+
+public class conersion {
+    public static void main(String[] args){
+        double dist_km=10.8;
+        double dist_miles=dist_km*1.6;
+        System.out.print("The distance "+dist_km+" km in miles is "+dist_miles+" miles");
+
+    }
+}
