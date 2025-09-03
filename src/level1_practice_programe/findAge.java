22fb6dadc3c9ae3d9f45764067bc61745fe9b626
rameshwar-dubey <rameshward040@gmail.com>
2025-09-03 12:43:54 +0530

    clear main branch

--- src/level1_practice_programe/findAge.java
+++
@@ -1,9 +0,0 @@
-package level1_practice_programe;
-public class findAge {
-    public static void main(String[] args) {
-        int birth_year = 2000;
-        int current_year = 2024;
-        int age = current_year - birth_year;
-        System.out.print(age);
-    }
-}
