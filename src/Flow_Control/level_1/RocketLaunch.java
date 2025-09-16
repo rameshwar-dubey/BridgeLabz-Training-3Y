6fedb71a6e42da583468cad5671cb60bddb8bd90
rameshwar-dubey <rameshward040@gmail.com>
2025-09-16 10:39:17 +0530

    added one file

---
+++ src/Flow_Control/level_1/RocketLaunch.java
@@ -0,0 +1,18 @@
+package Flow_Control.level_1;
+
+import java.util.Scanner;
+
+public class RocketLaunch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int counter = sc.nextInt();
+        while (counter >= 1) {
+            System.out.println(counter);
+            counter--;
+        }
+        System.out.println("Lift Off 🚀");
+
+        sc.close();
+    }
+}
+
