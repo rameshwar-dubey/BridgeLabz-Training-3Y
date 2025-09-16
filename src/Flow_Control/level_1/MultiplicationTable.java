8d9ab61fa9d3bb393e47a683a8a3bffb4be199b0
rameshwar-dubey <rameshward040@gmail.com>
2025-09-16 11:32:52 +0530

    added one file

---
+++ src/Flow_Control/level_1/MultiplicationTable.java
@@ -0,0 +1,19 @@
+package Flow_Control.level_1;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        System.out.println("Multiplication Table of " + number + " from 6 to 9:");
+
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+
+        sc.close();
+    }
+}
+
