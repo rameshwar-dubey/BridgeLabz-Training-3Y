dadac8559474655d4a198fb92411835db6c6ffc3
rameshwar-dubey <rameshward040@gmail.com>
2025-09-16 12:42:55 +0530

    added one file

---
+++ src/Flow_Control/level_3/HarshadNumber.java
@@ -0,0 +1,24 @@
+package Flow_Control.level_3;
+
+import java.util.Scanner;
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int sum = 0;
+        int temp = number;
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += digit;
+            temp = temp / 10;
+        }
+        if (number % sum == 0) {
+            System.out.println(number + " is a Harshad Number.");
+        } else {
+            System.out.println(number + " is not a Harshad Number.");
+        }
+
+        sc.close();
+    }
+}
