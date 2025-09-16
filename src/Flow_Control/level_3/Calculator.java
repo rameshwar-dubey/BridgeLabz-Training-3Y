a72c4701dcccd748d93ae871613db4b61eba08c8
rameshwar-dubey <rameshward040@gmail.com>
2025-09-16 12:47:02 +0530

    added one file

---
+++ src/Flow_Control/level_3/Calculator.java
@@ -0,0 +1,47 @@
+package Flow_Control.level_3;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double first = sc.nextDouble();
+        System.out.print("Enter the second number: ");
+        double second = sc.nextDouble();
+        System.out.print("Enter an operator (+, -, *, /): ");
+        String op = sc.next();
+        double result;
+        switch (op) {
+            case "+":
+                result = first + second;
+                System.out.println("Result: " + result);
+                break;
+
+                case "-":
+                result = first - second;
+                System.out.println("Result: " + result);
+                break;
+
+            case "*":
+                result = first * second;
+                System.out.println("Result: " + result);
+                break;
+
+            case "/":
+                if (second != 0) {
+                    result = first / second;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero!");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid Operator");
+        }
+
+        sc.close();
+    }
+}
+
