d111ba74d31fc7a099a1108cd498cfeb3fe8d093
rameshwar-dubey <rameshward040@gmail.com>
2025-09-16 11:29:26 +0530

    added one file

---
+++ src/Flow_Control/level_1/OddEvenNumbers.java
@@ -0,0 +1,24 @@
+package Flow_Control.level_1;
+
+import java.util.Scanner;
+
+public class OddEvenNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (number <= 0) {
+            System.out.println("Invalid input! Please enter a natural number greater than 0.");
+        } else {
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is Even");
+                } else {
+                    System.out.println(i + " is Odd");
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
+
