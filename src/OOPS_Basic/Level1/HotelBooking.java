c85ebc12fb01440b1a32c27f6d444869df7c0374
rameshwar-dubey <rameshward040@gmail.com>
2025-09-19 21:55:56 +0530

    added one file

---
+++ src/OOPS_Basic/Level1/HotelBooking.java
@@ -0,0 +1,47 @@
+package OOPS_Basic.Level1;
+
+public class HotelBooking {
+    private String guestName;
+    private String roomType;
+    private int nights;
+
+    public HotelBooking() {
+        this.guestName = "Unknown";
+        this.roomType = "Standard";
+        this.nights = 1;
+    }
+
+    public HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    public HotelBooking(HotelBooking other) {
+        this.guestName = other.guestName;
+        this.roomType = other.roomType;
+        this.nights = other.nights;
+    }
+
+    public void displayBooking() {
+        System.out.println("Guest Name: " + guestName);
+        System.out.println("Room Type : " + roomType);
+        System.out.println("Nights    : " + nights);
+    }
+
+    public static void main(String[] args) {
+        HotelBooking booking1 = new HotelBooking();
+        booking1.displayBooking();
+
+        System.out.println();
+
+        HotelBooking booking2 = new HotelBooking("Ramesh", "Deluxe", 3);
+        booking2.displayBooking();
+
+        System.out.println();
+
+        HotelBooking booking3 = new HotelBooking(booking2);
+        booking3.displayBooking();
+    }
+}
+
