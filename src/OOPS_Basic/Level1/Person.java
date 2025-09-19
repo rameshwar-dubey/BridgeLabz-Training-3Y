1c10639f093e079e1ce1d2249b90e445d35e6a6e
rameshwar-dubey <rameshward040@gmail.com>
2025-09-19 21:54:10 +0530

    added one file

---
+++ src/OOPS_Basic/Level1/Person.java
@@ -0,0 +1,42 @@
+package OOPS_Basic.Level1;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+        this.name = "Unknown";
+        this.age = 0;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(Person other) {
+        this.name = other.name;
+        this.age = other.age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Ramesh", 22);
+        p1.display();
+
+        Person p2 = new Person(p1);
+        p2.display();
+    }
+}
+
