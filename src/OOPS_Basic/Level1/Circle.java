5cac1bdf1bfc0175b796eb92afdd383d5ea38946
rameshwar-dubey <rameshward040@gmail.com>
2025-09-19 21:52:17 +0530

    added one file

---
+++ src/OOPS_Basic/Level1/Circle.java
@@ -0,0 +1,41 @@
+package OOPS_Basic.Level1;
+
+public class Circle {
+    private double radius;
+
+    public Circle() {
+        this(1.0);
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public void displayDetails() {
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + getArea());
+        System.out.println("Circumference: " + getCircumference());
+    }
+
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        c1.displayDetails();
+
+        System.out.println();
+        Circle c2 = new Circle(5.0);
+        c2.displayDetails();
+    }
+}
+
